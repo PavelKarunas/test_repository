1ebb7ac63b36967c52695053904ee77c07275f92
PavelKarunas <60423702+PavelKarunas@users.noreply.github.com>
2020-04-16 21:38:37 +0300

    Update failreport.groovy

--- vars/failreport.groovy
+++ vars/failreport.groovy
@@ -1,4 +1,4 @@
-def notifyFailed(String message) {
+def call(String message) {
   mail to: 'kps4k@yandex.ru',
       subject: "Status of pipeline: ${currentBuild.fullDisplayName}",
       body: "$message: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}"
